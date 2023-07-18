459880dc7cd83ae853fc90644cc6fbcd2e31dd7f
lilyraf <95680925+lilyraf@users.noreply.github.com>
2023-07-18 15:26:56 +0800

    2nd commit, before add next page

--- app/src/main/java/com/ict602/rdreport/MainActivity.java
+++ app/src/main/java/com/ict602/rdreport/MainActivity.java
@@ -3,12 +3,39 @@ package com.ict602.rdreport;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    GoogleSignInOptions gso;
+    GoogleSignInClient mGoogleSignInClient;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        updateUI(account);
+        if(account == null){
+            //null sign in
+            Toast.makeText(this,"Please sign in")
+        } else {
+            // user signed in already
+        }
     }
 }
