298d07989e57524a0e559754415ba251eecbc1cb
lilyraf <95680925+lilyraf@users.noreply.github.com>
2023-07-18 17:16:56 +0800

    complete google sign in

--- app/src/main/java/com/ict602/rdreport/MainActivity.java
+++ app/src/main/java/com/ict602/rdreport/MainActivity.java
@@ -2,15 +2,21 @@ package com.ict602.rdreport;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     GoogleSignInOptions gso;
     GoogleSignInClient mGoogleSignInClient;
@@ -18,6 +24,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Set the dimensions of the sign-in button.
+        SignInButton signInButton = findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        signInButton.setOnClickListener(this);
 
         // Configure sign-in to request the user's ID, email address, and basic
         // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
@@ -37,6 +47,44 @@ public class MainActivity extends AppCompatActivity {
 
         } else {
             // user signed in already
+            Toast.makeText(this," already", Toast.LENGTH_SHORT).show();
+
         }
     }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.sign_in_button) {
+            Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+            startActivityForResult(signInIntent, 10);
+        }
+    }
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == 10) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            //updateUI(null);
+            // Signed in successfully, show authenticated UI.
+            // Open new Activity
+            Toast.makeText(this,"User already signed in",Toast.LENGTH_SHORT).show();
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("Bakpo Nih", "signInResult:failed code=" + e.getStatusCode());
+            Toast.makeText(this,"tak jadi ni sign in", Toast.LENGTH_SHORT).show();
+            //updateUI(null);
+        }
+    }
+
 }
